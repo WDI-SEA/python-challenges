9acc0cb3a137e5f49f07cfd073a0df44ef01cc18
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-02-05 20:35:10 -0800

    Got problems 1 and 2 so far

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,12 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+initial_string = input("Type a sentence!")
+new_array = []
+
+for i in range(0, len(initial_string)):
+	new_array.insert(0, initial_string[i])
+
+reverse_string = ''.join(new_array)
+print(reverse_string)
