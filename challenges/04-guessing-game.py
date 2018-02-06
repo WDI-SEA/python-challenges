72ff5375f934f39cd15b850fe1a718b2f3b7e0b1
adamredwoods <adamredwoods@users.noreply.github.com>
2018-02-05 23:22:56 -0800

    python

--- challenges/04-guessing-game.py
+++ challenges/04-guessing-game.py
@@ -1,7 +1,22 @@
 from random import randint
 
 MIN = 1
-MAX = 5
+MAX = 500
 
 print("Guess a number between %d and %d" % (MIN, MAX))
-print("Delete me. Here's a random number:", randint(MIN, MAX))
+answer = randint(MIN, MAX)
+tries=0
+guess=0
+
+while (guess != answer):
+    guess = input(":")
+    guess = int(guess)
+
+    tries+=1
+
+    if(guess<answer):
+        print("The number is higher than %d" % (guess))
+    if(guess>answer):
+        print("The number is lower than %d" %(guess))
+
+print("You got it! Took you %d tries." %(tries))
