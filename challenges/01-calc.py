2e0c6d6f4d3b1c4d4271e24b0ae180d2aa9cd290
aishabraud92 <aishabraud@gmail.com>
2018-02-06 11:20:39 -0800

    initial commit

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -1,4 +1,32 @@
-# Use the `input()` function to prompt a user to enter something.
-# input() always returns a string value. If you ever want someone
-# to enter a number you have to use the `int()` function to convert
-# what they typed in to a string.
+def add(x, y):
+   return x + y
+def subtract(x, y):
+   return x - y
+def multiply(x, y):
+   return x * y
+def divide(x, y):
+   return x / y
+
+print("Select operation.")
+print("1. +")
+print("2. -")
+print("3. x")
+print("4. /")
+
+# take input from user
+choice = input("Choose a number(1,2,3,4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
+
