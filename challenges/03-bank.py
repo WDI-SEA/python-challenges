89e45195b2252bde63c76882305aa0cdb0871123
rallysport206 <phan.tony125@gmail.com>
2018-02-06 10:34:30 -0800

    2 of 4 done

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,7 @@
 print("Welcome to Chase bank.")
+def person():
+    method = input("what calculation would you like to do?(deposit, withdraw, check_balance): " )
+    age = input("How odl are you?\n")
+    age = int(age)
+    
 print("Have a nice day!")
-
