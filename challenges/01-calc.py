9dd7aa9d531daf9437879a8cd87f450881d6ea03
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-02-06 10:23:33 -0800

    commit

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,39 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+def getNum(prompt):
+	try:
+		num = int(input(prompt))
+	except:
+		print('This calculator only takes integers!')
+		num = getNum(prompt)
+	return num
+
+def calculator():
+	method = input("What calculation would you like to do? (add, sub, mult, div)\n")
+	validMethods = ['add', 'sub', 'mult', 'div']
+	if method.lower() in validMethods:
+		num1 = getNum("Enter the first number:\n")
+		num2 = getNum("Enter the second number:\n")
+		if method.lower() == 'add':
+			return num1+num2
+		elif method.lower() == 'sub':
+			return num1-num2
+		elif method.lower() == 'mult':
+			return num1*num2
+		else:
+			return num1/num2
+	else:
+		print(method+" is an invalid operation!")
+		calculator();
+
+print(calculator())
+
+# What calculation would you like to do? (add, sub, mult, div)
+# add
+# What is number 1?
+# 3
+# What is number 2?
+# 6
+# Your result is 9
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,46 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+def getAction():
+	action = input("What would you like to do? (deposit, withdraw, check_balance)\n").lower()
+	options = ['deposit', 'withdraw', 'check_balance']
+	if action in options:
+		return action
+	else:
+		print(action+" is not a valid choice!")
+		return getAction()
 
+def getDiff(action):
+	try:
+		return int(input("How much would you like to "+action+"?\n"))
+	except:
+		print("Invalid entry: please enter an amount!")
+		return getDiff(action)
+
+def giveBalance(balance):
+	return "Your current balance is "+str(balance)+"."
+
+def doneYet(bal):
+	decision = input("Are you done? (y/n)\n").lower()
+	if (decision == "yes") or (decision == "y"):
+		return "Thank you!"
+	elif (decision == "no") or (decision == "n"):
+		return bankTransaction(bal)
+
+def bankTransaction(currBalance):
+	bal = currBalance
+	print("Welcome to Chase bank!"+giveBalance(bal))
+	action = getAction()
+	if action == 'deposit':
+		diff = getDiff(action)
+		bal += diff
+		result = "Deposit complete! "
+	elif action == 'withdraw':
+		diff = getDiff(action)
+		bal -= diff
+		result = "Withdrawl complete! "
+	else:
+		result = "Ok. "
+	print(result+giveBalance(bal)+"\n")
+	doneYet(bal)
+
+
+print(bankTransaction(4000))
+	
