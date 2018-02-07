7c241a77398d781e66b7524788f38e5a01ce5d06
rallysport206 <phan.tony125@gmail.com>
2018-02-06 21:38:28 -0800

    updated

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,7 +1,14 @@
 print("Welcome to Chase bank.")
 def person():
-    method = input("what calculation would you like to do?(deposit, withdraw, check_balance): " )
-    age = input("How odl are you?\n")
-    age = int(age)
-    
-print("Have a nice day!")
+    action = input("what would you like to do?(deposit, withdraw, check_balance): " )
+    balance = 2500
+    withdraw = int (input('how much to withdraw?'))
+    age = input("how much would you like to deposit?")
+
+    if action == 'check_balance':
+        print('your balance is', balance)
+    elif action == 'withdraw':
+        print(withdraw)
+        print('your current balance is ')
+
+person()
