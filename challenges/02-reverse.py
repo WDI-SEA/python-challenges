9be9cfaf3c0bf7754bc0fe2bd23c388a21a0d24a
Ashton <ashchristie@gmail.com>
2018-02-10 18:28:59 -0800

    finished

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -1,8 +1,9 @@
-# Python offers several ways to reverse a String. This is a classic thing
-# that lots of people want to do. It's probably easy to look up this
-# answer on Stack Overflow.
-#
-# This website of 30 Python Tips and Tricks also happens to point out
-# several ways to reverse a string, and it's a good read!
-#
-# http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+phrase = 'The sun will come out one day'
+
+def string_reverse(string):
+    reversed_string = ''
+    for i in range(len(string)):
+        reversed_string += string[len(string)-i-1]
+    print(reversed_string)
+
+string_reverse(phrase)
