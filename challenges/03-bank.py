ea76749d5e00d2c0e168869898e7b8217ba01bd9
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2018-09-24 16:43:40 -0700

    finished the hw

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,36 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+balance = 0
 
+def deposit(amount):
+	global balance
+	balance += amount
+	return balance
+
+def withdraw(amount):
+	global balance 
+	balance -= amount
+	return balance
+
+def check_balance():
+	global balance
+	return balance
+
+print("Select operation.")
+print("1.Deposit")
+print("2.Withdraw")
+print("3.Check_balance")
+
+choice = input("Enter choice(1/2/3):")
+
+amount = int(input("Enter amount: "))
+
+if choice == "1":
+	print(amount,"+",balance,"=", deposit(amount))
+
+elif choice == "2":
+	print(amount,"-",balance,"=", withdraw(amount))
+
+elif choice == "3":
+	print(check_balance())
+
+else:
+	print("Invalid input")
--- challenges/04-guessing-game.py
+++ challenges/04-guessing-game.py
@@ -1,7 +1,19 @@
 from random import randint
 
-MIN = 1
-MAX = 5
+answer = randint(1, 10)
 
-print("Guess a number between %d and %d" % (MIN, MAX))
-print("Delete me. Here's a random number:", randint(MIN, MAX))
+print("I'm thinking of a number between 1 and 10.")
+
+while True:
+	try:
+		guess = int(input("Please guess my number:"))
+	except:
+		print("That's not a number")
+		continue
+	if guess < answer:
+		print("Haha! Too low!")
+	elif guess > answer:
+		print("Haha! Too high!")
+	else:
+		print("Aww, you got it.")
+		break
