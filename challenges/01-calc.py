3678365b2f8a89f39be678686c34c82b1d938e1d
Zach VanSomeren <zachvansomeren@Zachs-MacBook-Pro.local>
2018-09-24 15:24:05 -0700

    Challange 1, calculator created.  All functions operational

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,18 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+operation = input("What calculation would you like to do? (add, sub, mult, div)\n")
+num1 = int(input("What is number 1?\n"))
+num2 = int(input("What is number 2?\n"))
+
+if(operation == 'add'):
+  print(num1 + num2)
+elif(operation == 'sub'):
+  print(num1 - num2)
+elif(operation == 'mult'):
+  print(num1 * num2)
+elif(operation == 'div'):
+  print(num1 / num2)
+else:
+  print('Invalid operation')
