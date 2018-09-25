722ad5e3c76253fbe67f2ae21882af7b549f767b
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-24 17:12:54 -0700

    finish challenges

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,17 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+request = input('What operation do you want to use? (add, sub, mult, div) ')
+firstNum = float(input('What is the first number? '))
+secondNum = float(input('What is the second number? '))
+if (request == 'add'):
+    print(firstNum + secondNum)
+elif (request == 'sub'):
+    print(firstNum - secondNum)
+elif (request == 'mult'):
+    print(firstNum * secondNum)
+elif (request == 'div'):
+    print(firstNum / secondNum)
+else:
+    print('That was not a valid operation')
