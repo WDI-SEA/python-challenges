9dfc5a32bcc2e19db56c3bc67bea451e5319d984
DeusMalsith <justinrashallphotography@gmail.com>
2018-09-24 21:39:10 -0700

    Did two of the three challenges

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,5 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+for element in reversed('Hello'): print(element)
