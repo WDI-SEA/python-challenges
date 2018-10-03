7dcf79d92a5ec63870378956b3a72ea191ad499e
DeusMalsith <justinrashallphotography@gmail.com>
2018-10-03 09:02:06 -0700

    update

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,36 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+balance = 20000
+done = False
+
+print("Your current balance is", balance)
+
+while not done:
+    action = (input("What would you like to do? (withdraw, deposit, check_balance)")).lower()
+
+    while action not in ["withdraw", "deposit", "check_balance"]:
+        action = (input("What would you like to do? (withdraw, deposit, check_balance)")).lower()
+
+    if action == "check_balance":
+        print("Your current balance is", balance)
+    else:
+        amount = None
+        while not isinstance(amount, int):
+            try:
+                amount = int(input("How much would you like to {}?".format(action)))
+            except:
+                print("This is a bank, take it seriously!")
+
+        if action == "deposit":
+            balance += amount
+        else:
+            balance -= amount
+
+            if balance < 0:
+                balance = 0
+
+    response = (input("Cool, are you done yet?")).lower()
+
+    if response in ["yes", "yeah", "y", "yea", "mhm"]:
+        done = True
