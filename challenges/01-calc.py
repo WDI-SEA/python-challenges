2461330b86546e77a8da9c9a5db447b0d962e4d7
gtole342 <gtole342@gmail.com>
2019-09-24 09:17:58 -0700

    Submit

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,24 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+operation = input("What operation would you like to perform? (add, sub, mult, div)")
+if operation == "add":
+  num1 = input("What is number 1?")
+  num2 = input("What is number 2?")
+  result = num1 + num2
+elif operation == "sub":
+  num1 = input("What is number 1?")
+  num2 = input("What is number 2?")
+  result = num1 - num2
+elif operation == "mult":
+  num1 = input("What is number 1?")
+  num2 = input("What is number 2?")
+  result = num1 * num2
+elif operation == "div":
+  num1 = input("What is number 1?")
+  num2 = input("What is number 2?")
+  result = num1 / num2
+else:
+  print("Invalid operation type. Please try again")
+if result:
+  print(f'Your result is {result}.')
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,16 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
-
+balance = 4000
+print("Welcome\n")
+print("Your current balance is:\n")
+print(balance)
+operation = input("What would you like to do? (deposit, withdraw, check balance)\n")
+if operation == "deposit":
+  amount = input("How much would you like to deposit?")
+  balance = balance + amount
+elif operation == "withdraw":
+  amount = input("How much would you like to withdraw?")
+  balance = balance - amount
+elif operation == "check balance":
+  pass
+else:
+  print("Invalid operation. Please try again.")
+print(f"Your balance is {balance}")
