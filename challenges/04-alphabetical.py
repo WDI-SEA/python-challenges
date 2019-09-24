043e069b3a650c81e1cbcf807ee025c73c6040db
sabdi21 <sabdi21@hotmail.com>
2019-09-24 09:41:15 -0700

    completed problem 4

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -2,3 +2,12 @@
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
 
+
+# def sorted_string():
+string = input("Give a string to sort :) ")
+sorted = ''.join(sorted(string))
+print(f"Here's your string all sorted! {sorted}")
+   
+
+
+# sorted_string()
