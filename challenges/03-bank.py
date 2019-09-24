df068b2bf29bd9359c0eb55df94f4be70fc77582
pdangelo4 <paulsdangelo@gmail.com>
2019-09-24 09:32:27 -0700

    coding challenges

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,24 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+def deposit(self): 
+    amount=float(input("Enter amount to be Deposited: ")) 
+    self.balance += amount 
+    print("\n Amount Deposited:",amount) 
+
+def withdraw(self): 
+    amount = float(input("Enter amount to be Withdrawn: ")) 
+    if self.balance>=amount: 
+        self.balance-=amount 
+        print("\n You Withdrew:", amount) 
+    else: 
+        print("\n Insufficient balance  ") 
+
+def display(self): 
+    print("\n Net Available Balance=",self.balance) 
+
+   
+
+deposit() 
+withdraw() 
+display() 
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,7 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+a = "supercalifragilisticexpialidocious"
+print(''.join(sorted(a)))
+
