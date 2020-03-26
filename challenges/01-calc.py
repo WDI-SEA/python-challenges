0f60867e6c1a5405eb87af2ca4da70a3f3832b79
melissay94 <melissay94@yahoo.com>
2020-03-26 09:00:08 -0700

    First two answers

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,35 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+def get_user_input():
+  return input("Enter a number: ")
+
+def calculator():
+  calc_type = input("What calculation would you lie to do? (add, sub, mult, div\n")
+  valid_type = False
+  while(valid_type == False):
+    if (calc_type == "add"):
+      valid_type = True
+      num = input("What is number 1?")
+      num_2 = input("What is number 2?")
+      print(str(int(num) + int(num_2)))
+    elif (calc_type == "sub"):
+      valid_type = True
+      num = input("What is number 1?")
+      num_2 = input("What is number 2?")
+      print(str(int(num) - int(num_2)))
+    elif (calc_type == "mult"):
+      valid_type = True
+      num = input("What is number 1?")
+      num_2 = input("What is number 2?")
+      print(str(int(num) * int(num_2)))
+    elif (calc_type == "div"):
+      valid_type = True
+      num = input("What is number 1?")
+      num_2 = input("What is number 2?")
+      print(str(int(num) / int(num_2)))
+    else:
+      print("Not a valid calculation type. Try again (add, sub, mult, div")
+
+calculator()
--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,15 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+import math
+
+def reverse_string(user_input):
+  user_list = list(user_input)
+  for i in range(math.ceil(len(user_list)/2)):
+    user_list[i], user_list[-(i+1)] = user_list[-(i+1)], user_list[i]
+  
+  reversed = "".join(user_list)
+  print(reversed)
+
+user_input = input("Enter a string:")
+reverse_string(user_input)
