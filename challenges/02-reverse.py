9f62258e290df02608c10323f22451a4684685d9
azocher <anna.zocher@gmail.com>
2020-03-25 18:42:32 -0700

    pushing up az-solutions to hw

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,12 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+def reverse_string(string):
+  new_string = ""
+  for letter in string:
+    new_string = letter + new_string
+  
+  return new_string
+
+print(reverse_string("hello"))
