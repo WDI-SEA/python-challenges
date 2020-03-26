fa6a4e1f8f5829d3b58b29de6898cd598ce15e7e
melissay94 <melissay94@yahoo.com>
2020-03-26 11:09:15 -0700

    Finished last two problems

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,50 @@
+import random
+
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+user_account_balance = random.randint(100, 56000)
+
+def display_balance():
+  global user_account_balance
+  print("Your current balance is " + str(user_account_balance))
+
+def withdraw_amount(amount):
+  global user_account_balance
+  user_num = float(amount)
+  if (user_num < user_account_balance):
+    user_account_balance -= user_num
+  else:
+    print("I'm sorry, you don't have that amount in your account.")
+  
+    display_balance()
+  
+
+def deposit_amount(amount):
+  global user_account_balance
+  user_num = float(amount)
+
+  user_account_balance += user_num
+  display_balance()
+
+
+completed = False
+
+while(completed == False):
+  user_input = input("What would you like to do? (deposit, withdraw, check balance)")
+  if (user_input == "check balance"):
+    display_balance()
+  elif (user_input == "deposit"):
+    amount_wanted = input("What would you like to deposit?")
+    deposit_amount(amount_wanted)
+  elif(user_input == "withdraw"):
+    amount_wanted = input("What would you like to withdraw?")
+    withdraw_amount(user_input)
+  else:
+    print("Not a valid interaction")
+  
+  finished_input = input("Anything else?")
+  if (finished_input == "no"):
+    completed = True
+
+print("Have a nice day!")
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,44 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+import random
+
+def quick_sort(quick_list, start, end):
+  # If start point is greater than end point, that list is sorted and can be returned
+  if start >= end:
+    # Printing each returned list because it won't print outside the function despite returning it.
+    print("".join(quick_list))
+    return quick_list
+  
+  # Get a random index in our list to start our comparison around
+  pivot_idx = random.randrange(start, end+1)
+  # Get the element at that index for comparisons
+  pivot_element = quick_list[pivot_idx]
+
+  # Swap our random element with the last element
+  quick_list[end], quick_list[pivot_idx] = quick_list[pivot_idx], quick_list[end]
+
+  # Create a copy of the start index to manipulate it without losing our start value
+  start_pointer = start
+
+  # Go through the list
+  for index in range(start, end):
+    # Check if the current element is less than the pivot element
+    if quick_list[index] < pivot_element:
+      # If yes, swap that element with the element at start pointer
+      quick_list[index], quick_list[start_pointer] = quick_list[start_pointer], quick_list[index]
+      # Increase start pointer index to go to the next
+      start_pointer += 1
+  
+  # Swap element at the end index, which was placed at the pivot index earlier, with the element of our manipulated start index
+  quick_list[end], quick_list[start_pointer] = quick_list[start_pointer], quick_list[end]
+
+  # Now recursively call quick sort of the list in two parts. This will continue until it's become basically pairs that are easy to swap
+  quick_sort(quick_list, start, start_pointer - 1)
+  quick_sort(quick_list, start_pointer + 1, end)
+
+
+user_input = input("What would you like alphabetized?\n")
+user_list = list(user_input.lower())
+quick_sort(user_list, 0, len(user_list) - 1)
