7ce102d2e03f789f6b18a73411f09157a21df84d
dlew253 <dlew253@yahoo.com>
2020-03-30 00:55:35 -0700

    sending

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,19 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+operator = input('What do you wanna do?')
+num1 = int(input('first number go here\n'))
+num2 = int(input('secondd number go here\n'))
+result = 'bad operator'
+
+if operator == 'add':
+    result = num1 + num2
+elif operator == 'sub':
+    result = num1 - num2
+elif operator == 'multi':
+    result = num1 * num2
+elif operator == 'div':
+    result - num1 / num2
+print(f'Your result was ' {result}) 
--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,13 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+import math
+
+def reverse_string(user_input):
+    user_list - list(user_input)
+    for i in ranger(math.cal(len(user_list)/2)):
+        user_list[i], user_list[ -(i+1) = user_list[-(i+1)], user_list[i]]
+
+    reversed = "".join(user_list)
+  
