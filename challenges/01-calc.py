dc932fc29a158de474fbc0cfc9fa2bd1285b180b
davidvelichko52 <davidvelichko52@gmail.com>
2020-05-27 09:06:30 -0700

    hw

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,27 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+add = 'add'
+sub = 'sub'
+multi = 'multi'
+div = 'div'
+
+
+op = input('Enter a op')
+print(op)
+num1 = int(input('what is number 1'))
+print(num1)
+num2 = int(input('what is number 2'))
+print(num2)
+if op == add:
+    print(num1 + num2)
+if op == sub:
+    print(num1 - num2)
+if op == multi:
+    print(num1 * num2)
+if op == div:
+    print(num1 / num2)
+
+
+
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,20 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+currbal = 4000
+deposit = 'deposit'
+withdraw = 'withdraw'
+check_balance = 'check_balance'
+yes = 'yes'
+
+print('Your current balance is')
+print(currbal)
+action = input('What would you like to do? deposit, withdraw, check_balance?')
+if action == deposit:
+    depvalue = int(input('How much would you like to deposit?'))
+    print('Your current balance is', currbal + depvalue)
+    qustion = input('Are you done?')
+    if qustion == yes:
+        print('Thank you!')
+
+
