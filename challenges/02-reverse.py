0de2d4ff638fb89e22874e88363be9289722f27d
Jummay77 <hansangho@yahoo.com>
2020-05-27 17:19:31 -0700

    hw 8/10 complete

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,14 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+for elem in reversed('hello world'):
+    print(elem)
+
+
+input = 'hello world'
+result = ''
+for c in reversed(input):
+    result = result + classic
+
+print(result)
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,9 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+def alpha(input):
+    return str(sorted(input))
+
+user_str = input('What would you like the alphabetize? \n')
+print(user_str, 'alphabetized is ', alpha(user_str))
