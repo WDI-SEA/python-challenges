9fa2a0ece5f94f080cd13467d48febf43867a77d
zbarovsky <zachary.barovsky@gmail.com>
2020-08-01 11:09:06 -0700

    challenge one complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,27 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+print('What calculation would you like to do? add, sub, mult, div')
+math = input()
+
+print('Please enter first number')
+num_one = input()
+
+print('Please enter second number')
+num_two = input()
+
+if math == 'add':
+    print(int(num_one) + int(num_two))
+elif math == 'sub':
+    print(int(num_one) - int(num_two))
+elif math == 'mult':
+    print(int(num_one) * int(num_two))
+elif math == 'div':
+    print(int(num_one) / int(num_two))
+else:
+    print('Sorry, invalid input option, please try again')
+
+
+
