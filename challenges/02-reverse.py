86306bcc4a431de65013e028456e7868c1daf8e7
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-08-02 12:52:14 -0700

    did reverse and alphabetical

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -8,4 +8,10 @@
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
 
 
+def string_reverse(string):
+  new_string = ""
+  for letter in string:
+    new_string = letter + new_string
+  return new_string
 
+print(string_reverse("Joshua Danao"))
