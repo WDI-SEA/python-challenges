c0d4fb4ffed85cb0daf5715b0a6e61067b7406ea
Emily Krauss <e.krauss27@gmail.com>
2020-08-02 15:39:00 -0700

    calc done

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,18 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+print('What calculation would you like to do? (add, sub, mult, div')
+x = input()
+print('What is the first number?')
+y = input()
+print('What is the second number?')
+z = input('')
+if x == 'add':
+    print('The rusult of ', y, 'plus ' , z, 'is ', float(y) + float(z))
+elif x == 'sub':
+    print('The rusult of ', y, 'minus ' , z, 'is ', float(y) - float(z))
+elif x == 'mult':
+    print('The rusult of ', y, 'multiplied by ', z, 'is ', float(y) * float(z))
+elif x == 'div':
+    print('The rusult of ', y, 'divided by ', z, 'is ', float(y) / float(z))
