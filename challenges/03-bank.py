ade1f29f2a7d7ce2ad98e331f7d32039c31cc94f
zbarovsky <zachary.barovsky@gmail.com>
2020-08-02 14:02:41 -0700

    fixed up challange three to hold account amount

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -4,22 +4,24 @@ current_balance = 0
 
 print(f'Your current balance is {current_balance}')
 
-def bank_interaction(current_balance):
+def bank_interaction():
+    # global current_balance
     print("what would you like to do? (deposit, withdraw, check_balance")
     option = input()
     if option == 'deposit':
         print('How much would you like to deposit')
         deposit_added = input()
         print(f'Your new total is {int(current_balance) + int(deposit_added)}')
-        return current_balance == int(current_balance) + int(deposit_added)
+        return int(current_balance) + int(deposit_added)
     if option == 'withdraw':
         print('How much would you like to withdraw')
         deposit_subtracted = input()
         print(f'Your new total is {int(current_balance) - int(deposit_subtracted)}')
+        return int(current_balance) - int(deposit_subtracted)
     if option == 'check_balance':
         print(f'You currently have {current_balance}')
 
-bank_interaction(current_balance)
+current_balance = bank_interaction()
 
 print("Are you done? (yes or no)")
 answer = input()
@@ -28,4 +30,4 @@ if answer == 'yes':
     print("Have a nice day!")
 elif answer == 'no':
     print(f'current balance is: {current_balance}')
-    bank_interaction(current_balance = current_balance)
+    bank_interaction()
