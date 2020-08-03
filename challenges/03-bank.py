dd14b0597816e5ed580a32f2440656a25bb1234b
Noa Kim <noanonoa@gmail.com>
2020-08-03 11:07:39 -0700

    bank challenge completed

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,33 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+balance = 1000
+done = "no"
 
+print("Welcome to Chase bank.")
+
+def atm():
+  # get a user's balance
+  global balance
+  global done
+
+  # run indefinitely until user declares 'done'
+  while (done != "yes"):
+    # check user input and see what they want to do
+    ans = input("What would you like to do?\n")
+    # use user input to act accordingly
+    if (ans == "deposit"):
+      # ask user how much they want to deposit
+      amount = int(input("How much would you like to deposit?\n"))
+      # add deposit to balance
+      balance = balance + amount
+      # check and see if done
+      done = input("Are you done?\n")
+    elif (ans == "withdraw"):
+      amount = int(input("How much would you like to withdraw?\n"))
+      balance = balance - amount
+      done = input("Are you done?\n")
+    elif (ans == "balance"):
+      print(f"Your current balnace is {balance}.\n")
+      done = input("Are you done?\n")
+
+atm()
+
+print("Have a nice day!")
