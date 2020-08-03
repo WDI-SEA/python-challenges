1de36246fc4a1f2ff7605990a4e5a232034c856f
Emily Krauss <e.krauss27@gmail.com>
2020-08-03 09:07:21 -0700

    didn't finish

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,16 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+print("What would you like to do? (deposit, withdraw, check_balance")
+selection = input()
+balance = 0
+
+if selection == 'depocit':
+    print("How much would you like to deposit?")
+    ammount = input()
+    balance == balance + int(ammount)
+if selection == 'withdtraw':
+    print("How much would you like to withdraw?")
+    ammount = input()
+    balance == balance + int(ammount)
+if selection == 'check_balance':
+    print("Your balance is : $",balance,".")
 
