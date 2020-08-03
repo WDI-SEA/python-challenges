01f195889b8cb474975f02097bdce350349aac08
Noa Kim <noanonoa@gmail.com>
2020-08-03 13:55:46 -0700

    reverse challenge completed

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,18 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+
+some_string = "oo bla di oo bla da"
+print(f"Original string: {some_string}")
+
+def reverse_string():
+  rev_str = ""
+  index = len(some_string)
+  
+  while index > 0:
+    rev_str += some_string[index - 1]
+    index -= 1
+  print(f"Reversed string: {rev_str}")
+
+reverse_string()
