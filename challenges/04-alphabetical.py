0139ebdc73fe6666816ada0cdec82c6b481b4a67
Emily Krauss <e.krauss27@gmail.com>
2020-08-03 09:36:16 -0700

    num 4 done

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,9 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+print('Gimme a string of letters please.')
+string = input()
+sorted_string = sorted(string)
+new_string = "".join(sorted_string)
+print(new_string)
