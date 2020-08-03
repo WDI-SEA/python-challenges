21ccaa50aa21a50897563ed9af20b566de0e4b32
sinav <sinavameghi@gmail.com>
2020-08-03 10:53:57 -0700

    hw review

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,28 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+balance = 1000
+done = "no"
+
+def atm():
+  global balance
+  global done
+
+  while (done != "yes"):
+    ans = input("What would you like to do? You can 'deposit', 'withdraw', or write 'balance' to see your balance!\n")
+
+    if (ans == "deposit"):
+      amount = int(input("How much would you like to deposit?\n"))
+      balance = balance + amount
+      print(f"Your current balance is {balance}\n")
+      done = input("Are you done?\n")
+    elif (ans == "withdraw"):
+      amount = int(input("How much would you like to withdraw?\n"))
+      balance = balance - amount
+      print(f"Your current balance is {balance}\n")
+      done = input("Are you done?\n")
+    elif (ans == "balance"):
+      print(f"Your current balance is {balance}\n")
+      done = input("Are you done?\n")
+      
+atm()
