f08e8167b72b62d61e3799cfba42ae4442382278
Thomas Helderop <thomashelderop@Thomass-MacBook-Air.local>
2020-08-03 08:15:49 -0600

    finished problems but unclear on instructions

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,7 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+def user_input():
+    num = input("Enter a number: ")
+
+user_input()
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,6 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+user_string = input("Enter a random series of letters to be alphabetized: ")
+
+"".join(sorted(user_string))
