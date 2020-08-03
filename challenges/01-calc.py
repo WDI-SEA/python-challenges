10fe1ba60fbf0ce5f19b3e1efeac16a5db019d1a
nguyendm77 <nguyendm77@gmail.com>
2020-08-03 09:03:51 -0700

    submit challenge1

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,42 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+# add function
+def add(x,y):
+    return x+y
+
+# sub function 
+def sub(x ,y):
+    return x-y
+
+# mult function
+def mult(x,y):
+    return x*y
+
+# div function
+def div(x,y):
+    return x/y
+
+#main function 
+print("What calulation would you like to do?")
+print ("add | sub | mult | div")
+
+while True:
+    choice = input("Enter your choice: ")
+    # check the choice 
+    if choice in ("add", "sub", "mult", "div"):
+        num1 = float(input("What is your number 1?"))
+        num2 = float(input("What is your number 2?"))
+
+        if choice == "add":
+            print("Your result is ", add(num1,num2))
+        elif choice == "sub":
+            print("Your result is ", sub(num1,num2))
+        elif  choice == "mult":
+            print("Your result is ", mult(num1,num2))
+        elif choice == "div":
+            print("Your result is ", div(num1,num2))
+        break 
+    else:
+        print("Invalid Input") 
