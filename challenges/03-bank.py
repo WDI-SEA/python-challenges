2e30103a0e154e6b9c0f19a1da7adf5fed0dcf0c
Louis <lreillywillhight@gmail.com>
2020-08-03 09:33:44 -0700

    added dictionary feature to allow multiple users in single session

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -2,8 +2,9 @@ print("Welcome to Chase bank.")
 print("Have a nice day!")
 
 ##GLOBAL VARIABLES
-balance = 0
+defaultBalance = 0
 name = ""
+balanceDictionary = {'lou':'50', 'user':'100'}
 
 
 ##FUNCTIONS
@@ -21,36 +22,44 @@ def nextTransaction():
         nextTransaction()
 
 def transaction():
-    global balance
+    global name
+    global defaultBalance
+    global balanceDictionary
 
     print('Please enter your name: ')
 
     name = input()
 
+    if name in balanceDictionary:
+        print(f'username {name} found... proceeding to mode selection...')
+    else:
+        print(f'creating user {name} with balance ${defaultBalance}')
+        balanceDictionary[name] = defaultBalance
+        print(balanceDictionary)
+
     print(f"Hello, {name}. Please enter transaction type (balance, deposit, withdraw): ")
 
     mode = input()
-
 
     print(f'transaction type: *{mode}*')
 
     if mode == "balance":
         # global balance
-        print(f'Your current balance is {balance}...')
+        print(f'Your current balance is ${balanceDictionary[name]}...')
     elif mode == "deposit":
         print(f'Hello, {name}, please enter an amount to deposit: $')
         deposit = input()
-        balance = int(deposit) + balance
-        print(f'after depositing ${deposit}, your new balance is {balance}')
+        balanceDictionary[name] = int(deposit) + int(balanceDictionary[name])
+        print(f'after depositing ${deposit}, your new balance is ${balanceDictionary[name]}')
 
     elif mode == "withdraw":
         print(f'Hello, {name}, please enter an amount to withdraw: $')
         withdrawal = input()
-        if int(withdrawal) > balance:
+        if int(withdrawal) > int(balanceDictionary[name]):
             print('error: insufficient funds.')
-        elif int(withdrawal) < balance:
-            balance = balance - int(withdrawal)
-            print(f'After a withdrawal of ${withdrawal}, your balance is ${balance}')
+        elif int(withdrawal) <= int(balanceDictionary[name]):
+            balanceDictionary[name] = int(balanceDictionary[name]) - int(withdrawal)
+            print(f'After a withdrawal of ${withdrawal}, your balance is ${balanceDictionary[name]}')
         else:
             print('error in withdrawal transaction')
     else:
