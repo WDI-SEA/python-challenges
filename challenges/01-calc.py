c31de9275dd257b224b8c51bd55cf78ddaa70ea7
jimmygranadino <jimmy.granadino@gmail.com>
2020-08-03 09:07:45 -0700

    challenge 1 done

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -1,4 +1,24 @@
-# Use the `input()` function to prompt a user to enter something.
-# input() always returns a string value. If you ever want someone
-# to enter a number you have to use the `int()` function to convert
-# what they typed in to a string.
+1# user prompt
+operator = raw_input("What calculation would you like to do: add, sub, mult, div? ")
+
+# first number prompt
+first = int(input("whats the first number that will be used for this calculation? "))
+
+# second number prompt
+second = int(input("whats the first number that will be used for this calculation? "))
+
+# conditional to execute correct calculation
+if operator == "add":
+    print("{}+{}= ".format(first, second))
+    print(first+second)
+elif operator == "sub":
+    print("{}-{}= ".format(first, second))
+    print(first-second)
+elif operator == "mult":
+    print("{}*{}=".format(first, second))
+    print(first*second)
+elif operator == "div":
+    print("{}/{}=".format(first, second))
+    print(first/second)
+else:
+    print("there was an error calculating!")
