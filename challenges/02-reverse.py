bc69ef3906c7a5cee0cbe319ce89e357af751eea
nguyendm77 <nguyendm77@gmail.com>
2020-08-03 09:41:09 -0700

    saved

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -5,4 +5,12 @@
 # several ways to reverse a string, and it's a good read!
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
 
-
+print("Enter a string: ")
+str = "reverse_me" # initial string
+reversedString=[]
+# index = len(str) # calculate length of string and save in index
+# while index > 0: 
+#     reversedString += str[ index - 1 ] # save the value of str[index-1] in reverseString
+#     index = index - 1 # decrement index
+reversedstring=''.join(reversed(str))
+print(reversedString) # reversed stringc
