c0500706141954c5e76a9b4e8c2da13cf5d3b780
Nick Foxen <foxen.nicholas@gmail.com>
2020-08-04 15:10:33 -0700

    Challenge 1 complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,23 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+def calculator():
+  method = input("What method would you like to use?\n")
+  
+  x = int(input("What is the first number?\n"))
+  y = int(input("What is the secondnumber?\n"))
+
+  methods = {
+    "add": x + y,
+    "multiply": x * y,
+    "div": x / y,
+    "sub": x - y,
+  }
+
+  if (method in methods):
+    return methods[method]
+  else:
+    return print("Please choose the correct method.")
+
+
+print(calculator())
