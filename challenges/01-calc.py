83a7e36845e9829953cfc517141a1593395f0e7f
Emily Barwinczak <ebarw@MacBook-Air.local>
2021-12-21 21:51:20 -0500

    Deliverable -- 12.21

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,17 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+operation = input("What method would you like to use: addition, subtraction, multiplication, or division?")
+num1 = int(input("Number 1: "))
+num2 = int(input("Number 2: "))
+
+if operation == "add":
+    print(f"Answer: {num1 + num2}")
+elif operation == "subtraction":
+    print(f"Answer: {num1 - num2}")
+elif operation == "multiplication":
+    print(f"Answer: {num1 * num2}")
+elif operation == "division":
+    print(f"Answer: {num1 / num2}")
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,23 @@
 print("Welcome to Chase bank.")
+
+balance = 4000
+transaction = True
+
+while transaction == True:
+    print(f"Your current balance is {balance}")
+    response = (input("What would you like to do? (deposit, withdraw, check _balance) "))
+
+    if response == "deposit":
+        amount = int(input("How much would you like to deposit?"))
+        balance += amount
+        print(f"Your current balance is {balance}")
+    elif response == "withdraw":
+        amount = int(input("How much would you like to withdraw? "))
+        balance -= amount
+        print(f"Your current balance is {balance}")
+    else:
+        print("Are you done? (y/n)")
+
+
 print("Have a nice day!")
 
