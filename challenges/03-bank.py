310ac05f2cf32ca7e86f09b64f2a0d49e6114368
Goro <goro.ikeda.iyeki@gmail.com>
2021-12-22 00:07:41 -0500

    ch03 continue loop unclear, additionally continue status must be cleared before every loop...

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -21,21 +21,26 @@ def main ():
     
     while continue_status == "Y":
 
-        print("What would you like to do: \n 1) check_balance \n 2) deposit \n 3) widthdraw \n")
+        print("What would you like to do: \n 1) Check Account Balance \n 2) Withdraw \n 3) Deposit \n Please Enter a Number Choice: \n")
         transaction = int(input("> "))
+        print("\n")
 
         if transaction == 1:
-            print(current_session.get_balance())
+            print(f"Your current balance is: \t ${current_session.get_balance()}")
         elif transaction == 2:
-            amount = float(input("How much do you want to Withdraw?\n >"))
+            amount = float(input("How much do you want to Withdraw?\n> "))
             current_session.withdraw(amount)
-            print(current_session.get_balance())
+            print(f"Your current balance is: \t ${current_session.get_balance()}")
         elif transaction == 3:
-            amount = float(input("How much do you want to Deposit \n >"))
+            amount = float(input("How much do you want to Deposit \n> "))
             current_session.deposit(amount)
-            print(current_session.get_balance())
+            print(f"Your current balance is: \t ${current_session.get_balance()}")
 
-        continue_status = input("Do you want to Log Out? Press Y or N. \n > ")
+        print("\n")
+        continue_status = input("Do you want to Continue with your transaction? Press Y or N. \n > ")
+        if continue_status == "Y":
+            transaction = None
+
 
     
 main()
