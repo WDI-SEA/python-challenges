1cd3b76c7134064b78c68e9ced6e919a9a7b7202
paulinal3 <paulinal3@outlook.com>
2021-12-21 20:17:32 -0500

    challenge 3 completed

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,5 +1,26 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
 
-age = int((input('How old are you? ')))
-task = (input('What  would you like  to  do? (deposit, withdraw, check_balance): '))
+balance = 4000
+transaction = True
+
+print("Welcome to Chase bank.")
+while transaction == True:
+    print(f'Your current balance is: {balance}')
+    action = (input('What would you like to do today? (withdraw or deposit): '))
+
+    if action == 'deposit':
+        amount = int(input('Enter deposit amount: '))
+        balance += amount
+        print(f'Your new balance is {balance}')
+    elif action == 'withdraw':
+        amount = int(input('Enter withdrawal amount: '))
+        balance -= amount
+        print(f'Your new balance is {balance}')
+    else:
+        print('Please pick a transaction action!')
+
+    continue_action = input('Are you done? (y/n): ')
+
+    if continue_action == 'y':
+        transaction= False
+        print("Have a nice day!")
+        break
