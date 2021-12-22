e98effbd94ff7902339843ec74b6a3b031d91921
Mackmiller <mackmiller19@gmail.com>
2021-12-21 23:05:20 -0500

    challenge 2 complete

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,12 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+reverse = ''
+
+def reversify(str):
+    split_apart = list(str)
+    split_apart.reverse()
+    print(''.join(split_apart))
+
+reversify('reverse-reverse')
