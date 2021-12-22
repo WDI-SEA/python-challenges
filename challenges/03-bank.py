b5a91b5b9719fd525b8b54eb3b89298679f9b692
gsdkira <larreakelly@gmail.com>
2021-12-21 21:43:08 -0500

    alphabetical done

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,59 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+
+# ### Challenge 3 - Bank Transactions
+# Create a prompt that asks the user if they would like to display their balance,
+# withdraw or deposit. Write three methods to perform these calculations and
+# output the result to the user.
+
+# Gather user input using the `input` function. Note that input always returns
+# user input as a string. You have to manually convert it to an int or a float
+# to make it behave like number. Also, end the input prompt with a \n newline
+# character if you want the user to type in on the next line.
+
+# ```
+# age = input("How old are you?\n")
+# age = int(age)
+# ```
+
+# Here is a sample output:
+
+# ```
+# Your current balance is
+# 4000
+# What would you like to do? (deposit, withdraw, check_balance)
+# deposit
+# How much would you like to deposit?
+# 1000
+# Your current balance is 5000
+# Are you done?
+# yes
+# Thank you!
+
+
+# ```
+print("Welcome to Chase bank.")
+
+balance = 4000
+
+use_whattodo = input("What would you like to do? ")
+print(use_whattodo)
+if use_whattodo == "check balance":
+    print(f"Your balance is: {balance} ")
+elif use_howmuchdepo == "deposit":
+     use_howmuchdepo == int(input("How much would you like to deposit?"))
+     print(f"You deposited: ({use_howmuchdepo})")
+
+# use_howmuchdepo = int(input("How much would you like to deposit?")
+
+# if use_whattodo == 'deposit':
+#     print(use_howmuchdepo)
+    
+   
+
+
+# use_areyoudone = input("Are you done?")
+# print(use_areyoudone)
+
+
+
+print("Have a nice day!")
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,19 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+
+# Create a function that takes a string and returns the string with the letters in 
+# alphabetical order (ie. `hello` becomes `ehllo`), Assume numbers and punctuation 
+# symbols will not be included in the string.
+
+# ```
+# Give me a string to alphabetize
+# supercalifragilisticexpialidocious
+# Alphabetized: aaacccdeefgiiiiiiillloopprrssstuux
+# ```
+
+a_string = "kelly"
+new_string =  sorted(a_string)
+a_string = "".join(new_string)
+print(a_string)
