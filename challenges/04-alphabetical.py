98712d8811ec117b52b2f057fbbbf925a287c7c8
cmiltiadou <cmiltiad@gmail.com>
2021-12-22 00:52:36 -0500

    deliverable complete

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -2,3 +2,10 @@
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
 
+def alphabetize(string):
+    list = sorted(string)
+    new_string = ''.join(list)
+    print(new_string)
+
+alphabetize("what")
+
