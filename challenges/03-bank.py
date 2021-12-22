02bd4f94a3af25667839e5f617ba24a255e49444
paulinal3 <paulinal3@outlook.com>
2021-12-21 19:28:09 -0500

    challenge 4 completed

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,5 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+age = int((input('How old are you? ')))
+task = (input('What  would you like  to  do? (deposit, withdraw, check_balance): '))
