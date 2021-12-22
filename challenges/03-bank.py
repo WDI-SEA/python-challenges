457faac09b780a7a41cc42302d0ce462795b87e9
ornery-mouse <cory.sorel@gmail.com>
2021-12-21 20:06:43 -0600

    #3 done

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,20 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+def welcome():
+    transaction = ''
+    balance = 1000
+    print("Welcome to Chase bank.")
+    userInput = input('What would you like to do? (deposit, withdraw, check_balance)')
+    transaction = userInput
+    if transaction == 'deposit':
+        toDeposit = input('How much would you like to deposit?')
+        balance += int(toDeposit)
+        print(f"Your current balance is {balance}")
+    elif transaction == 'withdraw':
+        toWithdraw = input('How much would you like to withdraw?')
+        balance -= int(toWithdraw)
+        print(f"Here's your cash. Your current balance is {balance}")
+    elif transaction == 'check_balance':
+        print(f'Your current balance is {balance}')
+
+welcome()
+print("Have a nice day!")
