84831439333a818d0e8ea3cdc05db17c0a9fca53
anaismveras <verasmanais@gmail.com>
2021-12-21 19:25:56 -0500

    finished question 3

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,9 +1,37 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
-balance_question = input("Would you like to display your balance? (yes or no)\n")
-duties = input("Widthdraw or Deposit?\n")
+balance_question = input(
+    "Would you like to display your balance? (yes or no)\n")
 current_balance = 5000
 
-if (balance_question.lower() == 'yes'):
+def bank_transaction():
+    duties = input("Withdraw, Deposit or Check Balance\n")
+    if (duties.lower() == 'withdraw'):
+        withdraw = int(input('How much woudl you like to withdraw?\n'))
+        print('Your new current balance is', current_balance - withdraw)
+    elif (duties.lower() == 'deposit'):
+        deposit = int(input('How much woudl you like to Deposit?\n'))
+        print('Your new current balance is', current_balance + deposit)
+    elif (duties.lower() == 'check balance'):
+        print(current_balance)
+
+
+def finished():
+    done = input('Are You done?\n')
+    if (done.lower() == 'yes'):
+        print("Have a nice day!")
+    else:
+        bank_transaction()
+
+
+if balance_question.lower() == 'yes':
     print("Your current balance is\n", current_balance)
+    bank_transaction()
+    finished()
+else:
+    print("let's continue")
+    bank_transaction()
+    finished()
+
+
+
