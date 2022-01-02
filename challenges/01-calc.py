2d3b4cdcde6c0ad7894f916d1205395f6ecb52f7
Lawrencesalinas <lawrencesalinas@yahoo.com>
2022-01-02 06:21:42 -0500

    reverse.py completed

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,28 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+operation = input('what calculation would you like to do? (add, sub, mult, div)\n')
+num1 = int(input('what is number 1? \n'))
+num2 = int(input('what is number 2?\n'))
+
+if operation == 'add':
+    result = (num1 + num2)
+    print(f'Your result is {result}')
+elif operation == 'sub':
+    result = (num1 - num2)
+    print(f'Your result is {result}')
+elif operation == 'mult':
+    result = (num1 * num2)
+    print(f'Your result is {result}')
+elif operation == 'div':
+    result = (num1 / num2)
+    print(f'Your result is {result}')
+else:
+    print('invalid operator')
+
+
+
+
+
+
