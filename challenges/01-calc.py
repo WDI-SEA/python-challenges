3ad88ab7547ef66b61d3cf7dbf1e32354f2ff0a4
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-04-05 00:21:46 -0700

    finishing deliverable

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,26 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+print("What calculation would you like to do? (add, sub, mult, div)")
+calc_type = input()
+print("What is the first number?")
+num1 = input()
+print("What is the second number?")
+num2 = input()
+if calc_type == "add":
+    num_sum = int(num1) + int(num2)
+    print(f"The sum of both numbers is: {num_sum}")
+
+elif calc_type == "sub":
+    num_sub = int(num1) - int(num2)
+    print(f"The difference of both numbers is: {num_sub}")
+
+elif calc_type == "mult":
+    num_mult = int(num1) * int(num2)
+    print(f"The product of both numbers is: {num_mult}")
+
+else:
+    num_div = int(num1) / int(num2)
+    print(f"The dividend of both numbers is: {num_div}")
+
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,27 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+def chase_bank():
+    balance = 4000
+    print("what would you like to do? (deposit, withdraw, check_balance)")
+    user_choice = input()
+
+    if user_choice == "deposit":
+        print("How much would you like to deposit?\n")
+        deposit = input()
+        print(f"Your current balance is: {balance + int(deposit)}")
+    elif user_choice == "withdraw":
+        print("How much would like to withdraw?\n")
+        withdraw = input()
+        print(f"Your current balance is: {balance - int(withdraw)}")
+    elif user_choice == "check_balance":
+        print(f"Your current balance is: {balance}")
+
+    print("Are you done?")
+    choice2 = input()
+
+    if choice2 == "yes":
+        print("Have a nice day!")
+    else:
+        print("what would you like to do? (deposit, withdraw, check_balance)")
+
+chase_bank()
 
