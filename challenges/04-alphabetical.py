7d0ab0a176aca486d459430721415b0650439339
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-04-05 10:12:14 -0700

    finished last challenge

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,15 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+s = "lkjfsldkfjssjhfebglskfjgsaglsdgjfsaoinvfskafd"
+
+# convert to list
+li = list(s)
+
+# list .sort
+li.sort()
+# convert to a string
+text = "".join(li)
+# print it out
+print(text)
