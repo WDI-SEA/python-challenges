94e28b03e26c1507b88f7acd686b4772666222d4
stephanieonyekaba <onyek009@umn.edu>
2022-04-11 23:54:40 -0400

    calc and reverse string complete

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -1,4 +1,20 @@
-# Use the `input()` function to prompt a user to enter something.
-# input() always returns a string value. If you ever want someone
-# to enter a number you have to use the `int()` function to convert
-# what they typed in to a string.
+
+
+some_operation = input("please enter an operator: ")
+num1 = input("please enter a number:" )
+num2 = input("please enter a number2: ")
+
+if some_operation == 'add':
+    print(f"the sum of {num1} and {num2} is {int(num1) + int(num2)}" )
+
+elif some_operation == 'subtract':
+    print(f"the difference of {num1} and {num2} is {int(num1) - int(num2)}" )
+
+elif some_operation == 'multiply':
+    print(f"the product of {num1} and {num2} is {int(num1) * int(num2)}" )
+
+elif some_operation == 'divide':
+    print(f"the quotient of {num1} and {num2} is {int(num1) / int(num2)}" )
+
+else: print("invalid operation")
+
--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -1,8 +1,12 @@
-# Python offers several ways to reverse a String. This is a classic thing
-# that lots of people want to do. It's probably easy to look up this
-# answer on Stack Overflow.
-#
-# This website of 30 Python Tips and Tricks also happens to point out
-# several ways to reverse a string, and it's a good read!
-#
-# http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+
+
+a_string = input("please enter a string: ")
+an_array = list(a_string)
+new_array = []
+for char in an_array:
+    new_array.append(char)
+    list.reverse(new_array)
+    listToStr = ''.join(map(str, new_array))
+print(listToStr)
+
