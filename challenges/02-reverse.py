4a967559e403e7b6916fca991e8b4f030d6fa478
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-07-05 14:56:33 -0600

    Complete Reverse

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,12 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+user_input = input("Enter a string: ")
+
+reversed = ""
+
+for char in user_input:
+  reversed = char + reversed
+
+print(reversed)
