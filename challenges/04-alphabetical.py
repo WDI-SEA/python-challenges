b8aa19c075076d924cc2fa19324474984d629f9d
Hoan Le <hoanleecr@gmail.com>
2022-07-05 17:14:19 -0700

    finished the alphabetical

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,12 @@
-# You'll need to use a couple of built in functions to alphabetize a string. 
+# You'll need to use a couple of built in functions to alphabetize a string.
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+string = input('Give me a string to sort the alphabet')
+the_list = list(string)
+the_list.sort()
+
+alphabet = ''
+for char in the_list:
+    alphabet += char
+
+print(alphabet)
