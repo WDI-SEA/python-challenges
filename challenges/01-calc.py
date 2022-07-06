51ea574e10cad8919599fdb325ed02d9daa0c2a0
ASelviyan <angelikaselviyan5@gmail.com>
2022-07-05 18:25:25 -0600

    :done ^-^

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,29 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+def calc(): 
+    print('Would you like to add, subtract, multiply or divide')
+    method = input()
+    print('whats the first number?')
+    num1 = input()
+    print('whats the second number?')
+    num2 = input()
+  
+    if(method == 'add'):
+        total = float(num1) + float(num2)
+        return total
+    elif(method == 'subtract'):
+        total = float(num1) - float(num2)
+        return total
+    elif(method == 'multiply'):
+        total = float(num1) * float(num2)
+        return total
+    elif(method == 'divide'):
+        total = float(num1) / float(num2)
+        return total
+    else:
+        print('that is not a method')
+
+
+print(calc())
--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,17 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+txt = "Hello World"
+
+def reverseSTring(txt): 
+    reverse_txt = ''
+    for i in txt:
+        print(i)
+        reverse_txt = i + reverse_txt 
+        # olleH
+    return reverse_txt
+
+print(reverseSTring(txt))
+
+
