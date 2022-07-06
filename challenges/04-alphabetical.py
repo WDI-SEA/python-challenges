87c23ba6f6acafbda4bbbc0d4526f83d6ebca073
gracenarez333 <gracemnarez@gmail.com>
2022-07-06 09:15:32 -0700

    another method added to alphabet challenge

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -6,3 +6,12 @@ alphabet = "zyxwvutsrqonmlkjihgfedcba"
 arr_alphabet = list(alphabet)
 arr_alphabet.sort()
 print("".join(arr_alphabet))
+
+def solution(string):
+    # sorted_string = "".join(sorted(string, key=str.lower))
+    # return sorted
+    li = list(string)
+    li.sort()
+    return "".join(li)
+print(solution("cba"))
+print(solution("zxy"))
