2af1f015e546b22b94acd49c3bad6e2ea1d82af0
Hoan Le <hoanleecr@gmail.com>
2022-07-05 17:01:40 -0700

    lab 3/4 done

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,22 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+operation = input('please enter a operator:')
+num1 = input('please input a number:')
+num2 = input('please input a number: ')
+
+if operation == 'add':
+    print(f"the sums of {num1} and {num2} is {int(num1) + int(num2)}")
+
+elif operation == 'subtract':
+    print(f"the difference of {num1} and {num2} is {int(num1) - int(num2)}")
+
+elif operation == 'divide':
+    print(f"the division of {num1} and {num2} is {int(num1) / int(num2)}")
+
+elif operation == 'multiply':
+    print(f"the product of {num1} and {num2} is {int(num1) * int(num2)}")
+
+else:
+    print('no operation WRONG VERY WONG')
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,20 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
 
+transaction = input(
+    'What would you like to do? (deposit, withdraw, display)')
+balance = 4000
+
+if transaction == 'display':
+    print(f'current balance of {balance}')
+
+elif transaction == 'deposit':
+    amount_deposit = input('how much would you like to deposit')
+    print(f'you have a total amount of {int(balance) + int(amount_deposit)}')
+    print('Have a gooood day')
+elif transaction == 'withdraw':
+    amount_withdraw = input('how much would you like to withdraw')
+    print(f'you have a total amount of {int(balance) - int(amount_withdraw)}')
+    print('Have a gooood day')
+
+else:
+    print('invalid transaction! Try again!')
