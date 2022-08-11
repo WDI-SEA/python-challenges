672f877b08fc0dd55356b9b3d944510c9353a344
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-08-11 16:17:22 -0400

    finished deliverable

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,18 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+num1 = float(input('enter a number: '))
+operator = input('enter operator: ')
+num2 = float(input('enter another number: '))
+
+if operator == "+":
+  print(num1 + num2)
+elif operator == "-":
+  print(num1 - num2)
+elif operator == "*" or "x":
+  print(num1 * num2)
+elif operator == "/":
+  print(num1 / num2)
+elif operator == "%":
+  print(num1 % num2)
