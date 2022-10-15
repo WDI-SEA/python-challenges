62a886d1777a1be69ac7b4bdaf806e9565628cf7
elek8871 <elekmichelle@gmail.com>
2022-10-15 15:35:18 -0600

    finishes deliverable

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,34 @@
 print("Welcome to Chase bank.")
-print("Have a nice day!")
+
+def transactions(balance):
+    print(f'your current balance is {balance}')
+    user_input = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+    if user_input == "deposit":
+        deposit= int(input("How much would are you deposting?\n"))
+        balance += (deposit)
+        print(f'your new balance is: {balance}')
+        return balance
+    elif user_input == "withdraw":
+        withdraw = int(input("How much would you like to withdraw?\n"))
+        balance -= (withdraw)
+        print(f'your new balance: is {balance}\n')
+        return balance
+    elif user_input == "check_balance":
+        print(f'your account balance is: {balance}\n')
+        return balance
+    else:
+        print("please enter a valid choice\n")
+        transactions(2000)
+transactions(2000)
+
+signoff = input("will that be all for today? (y/n)?\n")
+if signoff ==  "y" or "Y":
+    print ("Thank You! Have a fantastic day!")
+elif signoff == "n" or "N":
+    transactions(2000)
+
+
+
+
+
 
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,7 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+string = "Its only a flesh wound"
+alp_string = sorted(string.lower())
+print("" .join(alp_string))
