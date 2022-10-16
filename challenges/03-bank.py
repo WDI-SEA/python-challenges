02f497e6eb6796c7827c065c198f38833005c1c6
JasonXChen <jasonxchen888@gmail.com>
2022-10-16 18:50:20 -0400

    Finish Challenge 3

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,36 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+done = False
+balance = 4000
+def deposit():
+    amount = input("How much would you like to deposit? \n")
+    global balance
+    balance += int(amount)
+    print_balance()
+def withdraw():
+    amount = input("How much would you like to withdraw? \n")
+    global balance
+    balance -= int(amount)
+    print_balance()
+def print_balance():
+    print(f"Your current balance is {balance}")
 
+print("Welcome to Chase bank.")
+print_balance()
+while (not done):
+    action = input("What would you like to do? (deposit, withdraw, check_balance \n")
+    if (action == "deposit"):
+        deposit()
+        isDone = input("Are you done? \n")
+        if (isDone == "yes"):
+            done = True
+    elif (action == "withdraw"):
+        withdraw()
+        isDone = input("Are you done? \n")
+        if (isDone == "yes"):
+            done = True
+    elif (action == "check_balance"):
+        print_balance()
+        isDone = input("Are you done? \n")
+        if (isDone == "yes"):
+            done = True
+
+print("Have a nice day!")
