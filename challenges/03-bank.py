0e8f32317d42cf3897cbecb08c9fd6ae14776b3b
syorn96 <syorn2014@gmail.com>
2022-10-16 13:08:06 -0700

    completes challeneg 3

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,30 @@
 print("Welcome to Chase bank.")
+
+
+account_balance = 4000
+print('Your current balance is')
+print(account_balance)
+
+bank_transaction = input('What would you like to do? (deposit, withdraw, check_balance)\n')
+
+if bank_transaction == 'deposit':
+    deposit = input('How much would you like to deposit?\n')
+    deposit = int(deposit)
+    account_balance += deposit
+    print('Your current balance is ' + str(account_balance))
+elif bank_transaction == 'withdraw':
+    withdraw = input('How much would you like to withdraw?\n')
+    withdraw = int(withdraw)
+    account_balance -= withdraw
+    print('Your current balance is ' + str(account_balance))
+else:
+    print('Your current balance is ' + str(account_balance))
+
+customer_service = input('Are you done?\n')
+
+if customer_service == 'yes':
+    print('Thank you!')
+else:
+    bank_transaction
 print("Have a nice day!")
 
