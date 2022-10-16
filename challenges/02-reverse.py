98fe85f1502539cc9c5af77741bc45d751474f73
syorn96 <syorn2014@gmail.com>
2022-10-16 12:48:42 -0700

    completes challenege 2

--- challenges/02-reverse.py
+++ challenges/02-reverse.py
@@ -6,3 +6,19 @@
 # several ways to reverse a string, and it's a good read!
 #
 # http://www.techbeamers.com/essential-python-tips-tricks-programmers/?utm_source=mybridge&utm_medium=blog&utm_campaign=read_more#tip1
+
+string = input('Enter a string:')
+# reverse_list = None
+# for i,char in enumerate(reversed(string)):
+#     print(char)
+
+def reverse_string(string):
+    new_string = ''
+    index = len(string)
+    while index:
+        index -=1
+        new_string += string[index]
+    return new_string
+
+print(reverse_string(string))
+
