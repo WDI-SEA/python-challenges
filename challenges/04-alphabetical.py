620bd4632b01d6ba2eaada0bdc3a0aed19db8d01
Moodybleu <meganbenn27@icloud.com>
2022-10-17 09:52:49 -0700

    Completes Challenge 4

--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,34 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+### Challenge 4 - Sort a String
+
+# Create a function that takes a string and returns the string with the letters in 
+# alphabetical order (ie. `hello` becomes `ehllo`), Assume numbers and punctuation 
+# symbols will not be included in the string.
+
+# ```
+# Give me a string to alphabetize
+# supercalifragilisticexpialidocious
+# Alphabetized: aaacccdeefgiiiiiiillloopprrssstuux
+# ```
+
+word = 'supercalifragilisticexpialidocious'
+# convert string to list
+word_li = list(word)
+
+# sort list
+word_li.sort()
+# convert list to string
+new_string = ''
+for char in word_li:
+    new_string += char
+
+print(new_string)
+
+# print(dir(list))
+
+# my_li = ['D', 'A', 'C', 'B']
+# my_li.sort()
+# print(my_li)
