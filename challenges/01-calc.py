2a9482041d5bc3592eb98a9d18206597d6082772
aimeepham14 <aimee.pham14@gmail.com>
2022-10-17 09:02:25 -0700

    worke on deliverable

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,23 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+user = input('Add, Subtract, Multiply, or Divide')
+
+num1 = input('Value 1')
+
+num2 = input ('Value 2')
+
+if (user == 'add'):
+    result = int(num1) + int(num2)
+if (user == 'subtract'):
+    result = int(num1) - int(num2)
+if (user == 'multiply'):
+    result = int(num1) * int(num2)
+if (user == 'divide'):
+    result = int(num1) / int(num2)
+
+
+
+input(f'{result}')
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,16 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+def atm():
+    global done
+    global balance_two
+
+    while(done != 'yes'):
+        response = input("What would you like to do at this atm?\n")
+
+        if(response  == 'check_balance'):
+            print("Your balance is", balance_two)
+        elif (response == 'withdraw'):
+            amount = int(input(f"How much would you like to {response}\n"))
+            balance_two -= amount
+            print("Your balance is now", balance_two)
