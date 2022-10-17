c3b3801ad27ea371175dc6c04073c9f281c26c67
mousesaver <sean.fan87@gmail.com>
2022-10-16 21:38:13 -0700

    Sean's homework

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,23 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+def calculator(operation, num1, num2):
+    if operation == 'add':
+        res = num1 + num2
+    elif operation == 'sub':
+        res = num1 - num2
+    elif operation == 'mult':
+        res = num1 * num2
+    elif operation == 'div':
+        if num2 == 0:
+            res = "Number 2 cannot be 0"
+        else:
+            res = num1 / num2
+    else:
+        res = "wrong operation input"
+    print("Your result is", res)
+
+operation = input("What calculation would you like to do? (add, sub, mult, div)\n")
+number1 = float(input("What is number 1?\n"))
+number2 = float(input("What is number 2?\n"))
+calculator(operation, number1, number2)
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,31 @@
+
+
+def bank_transanctions():
+    action = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+    global balance
+    if action == "deposit":
+        amount = float(input("How much would you like to deposit?\n"))
+        balance += amount
+        print(f'Your current balance is ${balance}')
+    elif action == "withdraw":
+        amount = float(input("How much would you like to withdraw?\n"))
+        if balance < amount:
+            print(f'You only have ${balance}')
+        else:
+            balance -= amount
+            print(f'Your current balance is ${balance}')
+    elif action == "check_balance":
+        print(f'Your current balance is ${balance}')
+    else:
+        print('Wrong action! Please choose deposit, withdraw, or check_balance')
+    more_action = input("Are you done?\n")
+    if more_action == "no":
+        bank_transanctions()
+    else:
+        print("Thank you!")
+
+
+balance = 0.00
 print("Welcome to Chase bank.")
 print("Have a nice day!")
-
+bank_transanctions()
