2f888959e907180d148e05ea9e8afefe5c06a056
iandab47 <dabinett.ian@gmail.com>
2022-10-17 11:20:50 -0500

    finished problem 1

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,34 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+from ast import operator
+
+
+prompt = '> '
+print('Enter the operation you wish to use: add, sub, mult, div')
+math = input(prompt)
+
+print('Enter the first number')
+input_one = input(prompt)
+print('Enter the second number')
+input_two = input(prompt)
+
+calculate = {
+  'add' : int(input_one) + int(input_two),
+  'sub' : int(input_one) - int(input_two),
+  'mult': int(input_one) * int(input_two),
+  'div' : int(input_one) / int(input_two)
+}
+
+operation = {
+  'add' : '+',
+  'sub' : '-',
+  'mult': '*',
+  'div' : '/',
+}
+
+if math in operation:
+  print(f'You calculated: {input_one} {operation[math]} {input_two} = {calculate[math]}')
+else:
+  print('Bad Input 🤨')
