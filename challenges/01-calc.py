ed0a534d0607f69d69ee4ffeb47c98221a3e6fc6
paulyjkim1 <paulyjkim1@gmail.com>
2023-02-05 21:26:52 -0800

    finishe deliv

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,19 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+num1 = int(input('1st Number: '))
+num2 = int(input('2nd Number: '))
+method = input('What calculation would you like to do? (add, sub, mult, div): ')
+
+if(method == 'add'):
+    print(num1 + num2)
+elif(method == 'sub'):
+    print(num1 - num2)
+elif(method == 'mult'):
+    print(num1 * num2)
+else:
+    print(num1 / num2)
+
+
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,44 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+currentBalance = 0
+again = ''
+
+def deposit():
+    value = input('How much would you like to deposit: ')
+    global currentBalance
+    currentBalance = currentBalance + int(value)
+    print('Your current balance is ', currentBalance)
+
+def withdraw():
+    value = input('How much would you like to withdraw: ')
+    global currentBalance
+    currentBalance = currentBalance - int(value)
+    print('Your current balance is ', currentBalance)
+
+def check_balance():
+    global currentBalance
+    print('Your current balance is ', currentBalance)
+
+
+def run():
+    action = input('What would you like to do? (deposit, withdraw, check_balance): ')
+    if(action == 'deposit'):
+        deposit()
+    elif(action == 'withdraw'):
+        withdraw()
+    else:
+        check_balance
+    global again    
+    again = input('Are you done(yes/no): ')
+
+
+
+run()
+if(again == 'no'):
+    run()
+else:
+    again = 'yes'
+    print('thank you')
+    
+
