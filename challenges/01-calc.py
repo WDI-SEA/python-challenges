f770bb0d17352d153ce0f6bd9c223669aafa3370
ninambash <ninambash@gmail.com>
2023-02-05 19:14:57 -0800

    updated

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -1,4 +1,7 @@
 # Use the `input()` function to prompt a user to enter something.
+birthYear = input('Enter your birth year')
 # input() always returns a string value. If you ever want someone
+age = 2023 - int(birthYear)
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+print(age)
--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,35 @@
 print("Welcome to Chase bank.")
+#age = input("how old are you?\n")
+#age = int(age)
+
+# show current balance
+current_balance = 4000
+
+def decision_maker():
+    decision  = input("What would you like to do? (deposit, withdraw, check_balance) \n")
+    if decision == 'deposit':
+        deposit =  int(input("How much would you like to deposit? "))
+        print("Your current balance is \n", current_balance + deposit) 
+ 
+    elif decision == 'check_balance':
+        print('Your current balance is \n', current_balance)
+  
+    elif decision == 'withdraw':
+        withdraw = int(input("How much would you like to withdraw? "))
+        if current_balance - withdraw >= 0:
+            print("Your current balance is \n", current_balance - withdraw)
+        else:
+            print("You don't have enough funds.")
+ 
+    else:
+        print("Invalid option.")
+
+while True:
+    decision_maker()
+    done = input("Are you done? (yes/no) ")
+    if done == 'yes':
+        print("Thank You!")
+        break
+
 print("Have a nice day!")
 
