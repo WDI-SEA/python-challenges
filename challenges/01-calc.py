63b8b49cf2f6d1c2943f7836f89e07c15120cea2
emilyjryan <emilyryanmac@gmail.com>
2023-02-06 09:34:24 -0800

    cleaned up code

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -3,30 +3,33 @@
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
 
-# input() for type of calc (add, sub, mult, div)
-calc = input('Welcome to calculator! What kind of calculation would you like to do? (add, subtract, multiply, divide)\n')
-print(f'Okay cool, so you want to {calc}')
+is_running = True
+print('Welcome to calculator!')
 
-first_num = input('What is the first number you want to use?\n')
-print(f'Alrighty then, your first number is {first_num}')
-first_num = int(first_num)
+while is_running:
+    calc = input('What kind of calculation would you like to do? (add, subtract, multiply, divide)\n> ')
 
-second_num = input('What is the second number you want to use?\n')
-print(f'Awesome, so your second number is {second_num}. Just give me a second while I think...')
-second_num = int(second_num)
+    first_num = int(input(f'Okay great, so you want to {calc} some numbers. What\'s the first number?\n> '))
 
-if calc == "add":
-    add_result = (first_num + second_num)
-    print(f'So turns out {first_num} plus {second_num} is {add_result}.')
-elif calc == "subtract":
-    sub_result = (first_num - second_num)
-    print(f'Okay I did some computing and {first_num} minus {second_num} is {sub_result}.')
-elif calc == "multiply":
-    mult_result = (first_num * second_num)
-    print(f'If you multiply {first_num} by {second_num} you get {mult_result}.')
-elif calc == "divide":
-    div_result = (first_num / second_num)
-    print(f'When you divide {first_num} by {second_num} you get {div_result}.')
-else:
-    print("Hmm, didn't catch that. Maybe check your spelling?")
+    second_num = int(input(f'Gotcha, {first_num}. What\'s the second number?\n> '))
+
+    if calc == "add":
+        add_result = (first_num + second_num)
+        print(f'So turns out {first_num} plus {second_num} is {add_result}.')
+    elif calc == "subtract":
+        sub_result = (first_num - second_num)
+        print(f'Okay I did some computing and {first_num} minus {second_num} is {sub_result}.')
+    elif calc == "multiply":
+        mult_result = (first_num * second_num)
+        print(f'If you multiply {first_num} by {second_num} you get {mult_result}.')
+    elif calc == "divide":
+        div_result = (first_num / second_num)
+        print(f'When you divide {first_num} by {second_num} you get {div_result}.')
+    else:
+        print("Hmm, didn't catch that. Maybe check your spelling?")
+
+    quit = input('Want to calculate something else? (yes/no)\n> ')
+    if quit == 'no':
+        print('See ya 👋🏼')
+        is_running = False
 
