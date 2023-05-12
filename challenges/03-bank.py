1c9b179d0c25edd0313432cc40f5afb900e39695
Powwow84 <pao.saelee84@gmail.com>
2023-05-11 21:22:51 -0700

    Python challenges hw

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,23 @@
 print("Welcome to Chase bank.")
 print("Have a nice day!")
 
+
+action = input("Chose an action: balance, withdraw, deposit\n" )
+user_balance = 4000
+
+if action == 'balance':
+    print(user_balance)
+elif action == "withdraw":
+    deposit = input("how much would you like to withdraw \n")
+    deposit_value = int(deposit)
+    print(user_balance - deposit_value)
+elif action == 'deposit':
+    withdraw = input("how much would you like to deposit \n")
+    withdraw_amount = int(withdraw)
+    print(user_balance + withdraw_amount)
+else:
+    print("invalid option")
+
+
+
+
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,11 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+def sort_string(string):
+    sorted_string = ''.join(sorted(string))
+    return sorted_string
+
+user_string = input("Give me a string\n")
+
+print(sort_string(user_string))
