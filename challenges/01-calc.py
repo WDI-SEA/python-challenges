3191efdb72fd4f17ecbeb03ccf5fa1ca8e3bd0a2
juanedcabrera <juanedcabrera@gmail.com>
2023-05-11 22:24:14 -0600

    Deliverable completed

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,28 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+
+calculation = input("What calculation would you like to do? (add, sub, mult, div): ")
+
+number1 = float(input("What is number 1? "))
+number2 = float(input("What is number 2? "))
+
+result = None
+
+if calculation == "add":
+    result = number1 + number2
+elif calculation == "sub":
+    result = number1 - number2
+elif calculation == "mult":
+    result = number1 * number2
+elif calculation == "div":
+    if number2 != 0:
+        result = number1 / number2
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid calculation method.")
+
+if result is not None:
+    print("Your result is", result)
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,11 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+def alphabetize_string(string):
+    alphabetized_string = ''.join(sorted(string))
+    return alphabetized_string
+
+input_string = input("Give me a string to alphabetize:\n")
+result = alphabetize_string(input_string)
+print("Alphabetized:", result)
