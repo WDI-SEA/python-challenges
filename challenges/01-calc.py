300e3ae9a3073282aa626b4ae8e668c0633203e6
Andrew <andrew@andrewsmith.org>
2023-05-12 13:04:17 -0400

    Complete deliverable

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,21 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+while True:
+
+    operation = input("What operation would you like to do? Input add, sub, mult, or div: ")
+
+    num1 = int(input("What's your first number? "))
+
+    num2 = int(input("What's your second number? "))
+
+    if operation == "add":
+        print("Your result is " + str(num1 + num2))
+    elif operation == "sub":
+        print("Your result is " + str(num1 - num2))
+    elif operation == "mult":
+        print("Your result is " + str(num1 * num2))
+    elif operation == "div":
+        print("Your result is " + str(num1 / num2))
+    
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,9 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+
+string = "teststring"
+string_list = list(string)
+string_list.sort()
+string = "".join(string_list)
+print(string)
