d6391829c8ebcf1ab2fe5b8c9d6d988b09d6bc1e
Heiner000 <garrett.heiner@gmail.com>
2023-05-11 22:09:48 -0700

    adds another way to do 03-bank

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,4 +1,4 @@
-balance = 100
+balance = 4000
 
 def banking():
     print("Welcome to Chase bank.")
@@ -20,4 +20,43 @@ def banking():
 
     print("Have a nice day!")
 
-print(banking())
+# print(banking())
+
+def deposit(amount):
+    global balance
+    balance += amount
+    return balance
+
+def withdraw(amount):
+    global balance
+    if amount <= balance: #check if sufficient balance exists
+        balance -= amount
+    else:
+        print("Insufficient balance.")
+    return balance
+
+def check_balance():
+    global balance
+    return balance
+
+while True:
+    print(f"Your current balance is {balance}.")
+    transaction = input("What would you like to do? (deposit, withdraw, check_balance)\n")
+
+    if transaction == "deposit":
+        amount = int(input("How much would you like to deposit?\n"))
+        balance = deposit(amount)
+        print(f"Your current balance is {balance}")
+    elif transaction == "withdraw":
+        amount = int(input("How much would you like to withdraw?\n"))
+        balance = withdraw(amount)
+        print(f"Your current balance is {balance}")
+    elif transaction == "check_balance":
+        print(f"Your current balance is {balance}")
+    else:
+        print("Not a valid input. Select deposit, withdraw, check_balance.\n")
+
+    done = input("Are you done? (yes, no)\n")
+    if done.lower() == "yes":
+        print("Thank you!")
+        break
